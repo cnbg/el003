a8e63a7c8de55c2344ec53c1aacded130579181f
chyngyz <nurchin@gmail.com>
2024-04-13 11:14:42 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AccordionComponent from '@/components/AccordionComponent.vue'
+import EditorComponent from '@/components/EditorComponent.vue'
+</script>
+
+<template>
+  <AccordionComponent />
+  <EditorComponent />
+</template>
+
+<style scoped>
+</style>
---
+++ src/renderer.js
@@ -0,0 +1,11 @@
+import '@/assets/base.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+app.use(PrimeVue, {
+    // unstyled: true,
+})
+app.mount('#app')
