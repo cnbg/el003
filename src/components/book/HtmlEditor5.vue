97c2385adc2796473905d84b32799c41a7cc4e0e
Zholborsbek Ysmaiyl uulu <63614518+Zhobk@users.noreply.github.com>
2024-06-01 19:21:27 +0600

    tinymce image and video upload enabled. tintranslation added

--- src/components/book/HtmlEditor5.vue
+++ src/components/book/HtmlEditor5.vue
@@ -1,10 +1,20 @@
+<template>
+  <div>
+    <div class="flex flex-wrap justify-end gap-5 mb-5">
+      <Button @click="save" icon="pi pi-save" :label="$t('general.save')" severity="success" />
+    </div>
+    <Editor
+      v-model="content"
+      api-key="hog2k60fzfxvtrc3efz1kjwed1ubbtzubmwnf61wk0ivspzw"
+      :init="editorConfig"
+    />
+  </div>
+</template>
+
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { useBookStore } from '../../stores/book';
-import  Editor  from '@tinymce/tinymce-vue';
-
-
-
+import Editor from '@tinymce/tinymce-vue';
 
 const props = defineProps({
   html: {
@@ -27,41 +37,55 @@ const save = () => {
 watch(() => props.html, (newVal) => {
   content.value = newVal;
 });
+
+const editorConfig = {
+  height: 'calc(100vh - 330px)',
+  plugins: 'print preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+  //toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media template link anchor codesample | ltr rtl',
+  automatic_uploads: false,
+  file_picker_types: 'image media',
+  file_picker_callback: (cb, value, meta) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', meta.filetype === 'image' ? 'image/*' : 'video/*');
+    input.onchange = function() {
+      const file = this.files[0];
+      const reader = new FileReader();
+      reader.onload = function() {
+        // Convert the file to a base64 string
+        const id = 'blobid' + (new Date()).getTime();
+        const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        const base64 = reader.result.split(',')[1];
+        const blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        // Call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+      reader.readAsDataURL(file);
+    };
+    input.click();
+  },
+  extended_valid_elements: '*[.*]',
+  draggable_modal: true,
+  skin: 'oxide-dark',
+  content_css: 'dark',
+  statusbar: false,
+  language: 'ru',
+  setup: (editor) => {
+    editor.on('init', () => {
+      // Import the translation file after the editor is initialized
+      import('../../locale/ru/editor.js').then(() => {
+        // Translation file loaded successfully
+      
+      }).catch((error) => {
+        console.error('Failed to load translation file:', error);
+      });
+    });
+  },
+};
 </script>
 
-<template>
-  <div>
-    <div class="flex flex-wrap justify-end gap-5 mb-5">
-      <Button @click="save" icon="pi pi-save" :label="$t('general.save')" severity="success" />
-    </div>
-    <Editor
-      v-model="content"
-      api-key="hog2k60fzfxvtrc3efz1kjwed1ubbtzubmwnf61wk0ivspzw"
-      :init="{
-        height: 'calc(100vh - 330px)',
-        plugins: 'print preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-        toolbar: 'undo redo | bold italic underline strikethrough |tiny_mce_wiris_formulaEditor tiny_mce_wiris_formulaEditorChemistry| fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media template link anchor codesample | ltr rtl',
-        selector: 'textarea', 
-        external_plugins: {
-        tiny_mce_wiris: '../../../node_modules/@wiris/mathtype-tinymce5/plugin.min.js'}, 
-        extended_valid_elements: '*[.*]',
-        draggable_modal: true,
-        skin: 'oxide-dark',
-        content_css: 'dark',
-        statusbar: false,
-        language_load: true,
-        language_url: '../../locale/ru/editor.js', 
-        language: 'ru',
-        
-        /* plugins: 'media',
-        toolbar: 'media' */
-        
-      }"
-    />
-  </div>
-</template>
-
 <style>
-
-
+/* Add your custom styles here */
 </style>
