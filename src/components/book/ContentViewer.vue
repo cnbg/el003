e7ac214fe0a35abadc887a1a2de18f6fe722625e
Zholborsbek Ysmaiyl uulu <63614518+Zhobk@users.noreply.github.com>
2024-06-27 15:27:20 +0600

    fixed chapters not being shown

--- src/components/book/ContentViewer.vue
+++ src/components/book/ContentViewer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col gap-8">
-    <div v-for="(block, index) in chapter.blocks" :key="index">
+    <div v-for="(block, index) in chapter?.blocks" :key="index">
       <HtmlViewer v-if="block.type === 'html'" :html="block.content" @content-updated="updateContent(index, $event)" />
       <ImageViewer v-else-if="block.type === 'image'" :images="block.content" @image-updated="updateImage(index, $event)" @image-deleted="deleteImage(index, $event)" />
       <VideoViewer v-else-if="block.type === 'video'" :video="block.content" @content-updated="updateVideo(index, $event)" @delete-video="deleteVideo(index)" />
@@ -11,38 +11,37 @@
 </template>
 
 <script setup>
-  import { useBookStore } from '../../stores/book';
-  import { toRefs } from 'vue';
+defineProps(['chapter'])
+import { useBookStore } from '../../stores/book'
+const bookStore = useBookStore()
 
-  const { chapter } = toRefs(useBookStore());
+const updateContent = (index, updatedContent) => {
+  bookStore.updateBlockContent(index, updatedContent);
+};
 
-  const updateContent = (index, updatedContent) => {
-    useBookStore().updateBlockContent(index, updatedContent);
-  };
+const updateImage = (index, { index: imageIndex, image }) => {
+  bookStore.updateImageBlock(index, imageIndex, image);
+};
 
-  const updateImage = (index, { index: imageIndex, image }) => {
-    useBookStore().updateImageBlock(index, imageIndex, image);
-  };
+const deleteImage = (index, imageIndex) => {
+  bookStore.deleteImageBlock(index, imageIndex);
+};
 
-  const deleteImage = (index, imageIndex) => {
-    useBookStore().deleteImageBlock(index, imageIndex);
-  };
+const updateVideo = (index, updatedVideo) => {
+  bookStore.updateVideoBlock(index, updatedVideo);
+};
 
-  const updateVideo = (index, updatedVideo) => {
-    useBookStore().updateVideoBlock(index, updatedVideo);
-  };
+const deleteVideo = (index) => {
+  bookStore.deleteVideoBlock(index);
+};
 
-  const deleteVideo = (index) => {
-    useBookStore().deleteVideoBlock(index);
-  };
+const updateModel = (index, updatedModel) => {
+  bookStore.updateModelBlock(index, updatedModel);
+};
 
-  const updateModel = (index, updatedModel) => {
-    useBookStore().updateModelBlock(index, updatedModel);
-  };
-
-  const deleteModel = (index) => {
-    useBookStore().deleteModelBlock(index);
-  };
+const deleteModel = (index) => {
+  bookStore.deleteModelBlock(index);
+};
 </script>
 
 <style scoped>
